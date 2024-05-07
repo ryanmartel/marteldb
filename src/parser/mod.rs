e8f14ebc72346137c4ca70ff3f28bedd0e32ee38
ryanmartel <martel.private@gmail.com>
2024-05-06 19:15:13 -0700

    first commit

---
+++ src/parser/mod.rs
@@ -0,0 +1,8 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub calculator1, "/parser/calculator1.rs");
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+}
