ce3a485ea2135d5edef7e9ec62c6baa17755926b
ryanmartel <martel.private@gmail.com>
2024-06-05 21:31:25 -0700

    adding to ast

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -39,7 +39,24 @@ pub struct DropTableStmt {
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct SelectStmt {
+    pub distinct: bool,
+    pub results: Vec<ResultCol>,
+    pub from: Option<FromClause>,
+    pub filter: Option<WhereClause>,
+    pub group_by: Option<GroupByClause>,
+    pub except: Option<Box<SelectStmt>>,
+    pub order_by: Option<OrderByClause>,
+    pub limit: Option<LimitClause>,
+}
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct FromClause {
+    pub kind: FromClauseKind,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum FromClauseKind {
+    Join
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -58,6 +75,19 @@ pub struct UpdateStmt {
 }
 
 #[derive(Clone, Debug, PartialEq)]
+pub struct ResultCol {
+    pub kind: ResultColKind,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ResultColKind {
+    // * || tableName.*
+    All(Option<Ident>),
+    // expr >> (AS colum-alias)?
+    ExprRes(Box<Expr>, Option<Ident>)
+}
+
+#[derive(Clone, Debug, PartialEq)]
 pub struct Expr {
     pub kind: ExprKind,
 }
@@ -68,11 +98,47 @@ impl Expr {
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum ExprKind {
+    // Literal Value
     Lit(LiteralValue),
+    // expr >> BinOp >> expr
     Binary(BinOp, Box<Expr>, Box<Expr>),
+    // UnOp >> expr
     Unary(UnOp, Box<Expr>),
+    // tableName >> columnName || columnName
     Column(Option<Ident>, Ident),
+    // Expr, ...
     ExprList(Vec<Expr>),
+    // expr >> IS NOT? >> expr
+    ExprIS(Box<Expr>, NOTVal, Box<Expr>),
+    // expr >> NOT? BETWEEN >> expr >> AND expr
+    ExprBetween(Box<Expr>, NOTVal, Box<Expr>, Box<Expr>),
+    // expr >> NOT? IN >> InExpr
+    ExprIN(Box<Expr>, NOTVal, Box<InExpr>),
+    // NOT? Exists? >> Select
+    Exists(NOTVal, ExistsVal, Box<SelectStmt>),
+    
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct InExpr {
+    pub kind: InExprKind,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum InExprKind {
+    Select(Box<SelectStmt>),
+    ExprList(Vec<Expr>),
+    Table(Ident),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct NOTVal {
+    pub not: bool,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ExistsVal {
+    pub exists: bool,
 }
 
 #[derive(Clone, Debug, PartialEq)]
