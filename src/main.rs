a03d16d90ab6fefe1f512988175a48126694f972
Ryan Martel <martel.private@gmail.com>
2024-07-21 20:56:22 -0700

    finished adding literal values to grammar

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use marteldb::parser::grammar::ScriptParser;
 fn main() {
     let source = "INSERT dave;
     -- this is a comment
-    Insert 42;
-    Insert False;";
+    Insert dan = 42;
+    Insert kyle = False;
+    Insert bob = dan;";
     let lexer = Lexer::new(source);
     let parser = ScriptParser::new();
     let ast = parser.parse(lexer).unwrap();
