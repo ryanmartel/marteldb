87292df3f20c55dc7add473f31c9764378360960
Ryan Martel <martel.private@gmail.com>
2024-08-05 21:51:30 -0700

    added hello-world version of clap repl

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
-use marteldb::parser::lexer::Lexer;
-use marteldb::parser::grammar::ScriptParser;
-use marteldb::parser::prettyprinter::PrettyPrinter;
-use marteldb::parser::visitor::*;
+// use marteldb::parser::lexer::Lexer;
+// use marteldb::parser::grammar::ScriptParser;
+// use marteldb::parser::prettyprinter::PrettyPrinter;
+// use marteldb::parser::visitor::*;
+//
+use std::io::Write;
+use marteldb::repl;
 
-fn main() {
-    let source = "SELECT tab.col, tab.col2, tab.col3 FROM tab1, tab2 WHERE this AND  NOT that;
-    -- this is a comment
-    INSERT INTO tab1(col1, col2, col3) VALUES ('a', 10, FALSE);
-    CREATE TABLE tab3();
-    INSERT INTO tab2 VALUES('b', 20, NULL);";
-    let lexer = Lexer::new(source);
-    let parser = ScriptParser::new();
-    let ast = parser.parse(lexer).unwrap();
 
-    for i in &ast {
-        let mut pp = PrettyPrinter::new();
-        pp.visit_stmt(i);
-        // println!("{}", 1);
+fn main() -> Result<(), String> {
+    loop {
+        let line = repl::readline()?;
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+
+        match repl::respond(line) {
+            Ok(quit) => {
+                if quit {
+                    break;
+                }
+            }
+            Err(err) => {
+                write!(std::io::stdout(), "{err}").map_err(|e| e.to_string())?;
+                std::io::stdout().flush().map_err(|e| e.to_string())?;
+            }
+        }
     }
-    println!("{:#?}", ast);
+
+    Ok(())
+
+    // let source = "SELECT tab.col, tab.col2, tab.col3 FROM tab1, tab2 WHERE this AND  NOT that;
+    // -- this is a comment
+    // INSERT INTO tab1(col1, col2, col3) VALUES ('a', 10, FALSE);
+    // CREATE TABLE tab3();
+    // INSERT INTO tab2 VALUES('b', 20, NULL);";
+    // let lexer = Lexer::new(source);
+    // let parser = ScriptParser::new();
+    // let ast = parser.parse(lexer).unwrap();
+    //
+    // for i in &ast {
+    //     let mut pp = PrettyPrinter::new();
+    //     pp.visit_stmt(i);
+    //     // println!("{}", 1);
+    // }
+    // println!("{:#?}", ast);
 
 }
 
---
+++ src/repl.rs
@@ -0,0 +1,43 @@
+use std::io::Write;
+use clap::{Parser, Subcommand};
+
+pub fn respond(line: &str) -> Result<bool, String> {
+    let args = shlex::split(line).ok_or("error: Invalid quoting")?;
+    let cli = Cli::try_parse_from(args).map_err(|e| e.to_string())?;
+    match cli.command {
+        Commands::Ping => {
+            write!(std::io::stdout(), "Pong").map_err(|e| e.to_string())?;
+            std::io::stdout().flush().map_err(|e| e.to_string())?;
+        }
+        Commands::Exit => {
+            write!(std::io::stdout(), "Exiting ...").map_err(|e| e.to_string())?;
+            std::io::stdout().flush().map_err(|e| e.to_string())?;
+            return Ok(true);
+        }
+    }
+    Ok(false)
+}
+
+#[derive(Parser, Debug)]
+#[command(multicall = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Ping,
+    Exit,
+}
+
+pub fn readline() -> Result<String, String> {
+    write!(std::io::stdout(), "> ").map_err(|e| e.to_string())?;
+    std::io::stdout().flush().map_err(|e| e.to_string())?;
+    let mut buffer = String::new();
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .map_err(|e| e.to_string())?;
+    Ok(buffer)
+}
