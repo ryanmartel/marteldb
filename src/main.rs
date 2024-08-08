40dd4c8ba4bb4e06e8cb13093db40e504585066c
Ryan Martel <martel.private@gmail.com>
2024-08-08 07:53:09 -0700

    repl commands parsing correctly with stmts

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,10 @@ use marteldb::parser::lexer::Lexer;
 use marteldb::parser::grammar::ScriptParser;
 use marteldb::parser::prettyprinter::PrettyPrinter;
 use marteldb::parser::visitor::*;
-use clap::{Parser, Subcommand};
-//
+use clap::{error::ErrorKind, Parser, Subcommand};
+
 use std::io::Write;
 
-pub fn readline() -> Result<String, String> {
-    write!(std::io::stdout(), "MartelDB > ").map_err(|e| e.to_string())?;
-    std::io::stdout().flush().map_err(|e| e.to_string())?;
-    let mut buffer = String::new();
-    std::io::stdin()
-        .read_line(&mut buffer)
-        .map_err(|e| e.to_string())?;
-    Ok(buffer)
-}
 
 
 fn main() -> Result<(), String> {
@@ -39,7 +30,6 @@ fn main() -> Result<(), String> {
                         for i in &ast {
                             let mut pp = PrettyPrinter::new();
                             pp.visit_stmt(i);
-                            // println!("{}", 1);
                         }
                     }
                 }
@@ -49,33 +39,33 @@ fn main() -> Result<(), String> {
                 std::io::stdout().flush().map_err(|e| e.to_string())?;
             }
         }
-        // let v: Vec<&str> = line.split(' ').collect();
-        // if line.eq("exit") {
-        //     break;
-        // }
-
-        // let lexer = Lexer::new(line);
-        // let parser = ScriptParser::new();
-        // let ast = parser.parse(lexer).unwrap();
-        //
-        // for i in &ast {
-        //     let mut pp = PrettyPrinter::new();
-        //     pp.visit_stmt(i);
-        //     // println!("{}", 1);
-        // }
     }
 
     Ok(())
 }
 
+fn readline() -> Result<String, String> {
+    write!(std::io::stdout(), "MartelDB > ").map_err(|e| e.to_string())?;
+    std::io::stdout().flush().map_err(|e| e.to_string())?;
+    let mut buffer = String::new();
+    std::io::stdin()
+        .read_line(&mut buffer)
+        .map_err(|e| e.to_string())?;
+    Ok(buffer)
+}
+
 fn respond(line: &str) -> Result<Response, String> {
     let args = shlex::split(line).ok_or("error: Invalid quoting")?;
-    let cli_res = Cli::try_parse_from(args).map_err(|e| e.to_string());
+    let cli_res = Cli::try_parse_from(args);
     match cli_res {
         Ok(cli) => {
             match cli.command {
                 Commands::Ping => {
-                    write!(std::io::stdout(), "Pong").map_err(|e| e.to_string())?;
+                    writeln!(std::io::stdout(), "Pong").map_err(|e| e.to_string())?;
+                    std::io::stdout().flush().map_err(|e| e.to_string())?;
+                }
+                Commands::Init { name } => {
+                    writeln!(std::io::stdout(), "Initializing database {name}").map_err(|e| e.to_string())?;
                     std::io::stdout().flush().map_err(|e| e.to_string())?;
                 }
                 Commands::Exit => {
@@ -85,14 +75,26 @@ fn respond(line: &str) -> Result<Response, String> {
                 }
             }
         },
-        // No metacommand
-        Err(_) => {
-            return Ok(Response::Stmt);
+        // Not valid metacommand
+        Err(err) => {
+            match err.kind() {
+                ErrorKind::InvalidSubcommand => {
+                    return Ok(Response::Stmt);
+                }
+                ErrorKind::UnknownArgument => {
+                    return Ok(Response::Stmt);
+                }
+                // Other type of CLI error, such as missing arguments to metacommand
+                _ => {
+                    return Err(err.to_string());
+                }
+            }
         }
     }
     Ok(Response::MetaCommand)
 }
 
+#[derive(Debug)]
 enum Response {
     Quit,
     MetaCommand,
@@ -109,6 +111,11 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Commands {
     Ping,
+
+    Init {
+        name: String,
+    },
+
     Exit,
 }
     // let source = "SELECT tab.col, tab.col2, tab.col3 FROM tab1, tab2 WHERE this AND  NOT that;
--- src/repl.rs
+++ src/repl.rs
@@ -1,28 +1,109 @@
 use std::io::Write;
 use clap::{Parser, Subcommand};
 
-pub fn respond(line: &str) -> Result<bool, String> {
-    let args = shlex::split(line).ok_or("error: Invalid quoting")?;
-    let cli = Cli::try_parse_from(args).map_err(|e| e.to_string())?;
-    match cli.command {
-        Commands::Ping => {
-            write!(std::io::stdout(), "Pong").map_err(|e| e.to_string())?;
-            std::io::stdout().flush().map_err(|e| e.to_string())?;
+pub struct Repl {
+
+}
+impl Repl {
+
+    fn new() -> Self {
+
+    }
+
+    fn handle_input (&self) -> Response {
+        let line = readline()?;
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
         }
-        Commands::Exit => {
-            write!(std::io::stdout(), "Exiting ...").map_err(|e| e.to_string())?;
-            std::io::stdout().flush().map_err(|e| e.to_string())?;
-            return Ok(true);
+        match respond(line) {
+            Ok(resp) => resp,
+            Err(err) => {
+                write!(std::io::stdout(), "{err}").map_err(|e| e.to_string())?;
+                std::io::stdout().flush().map_err(|e| e.to_string())?;
+            }
         }
     }
-    Ok(false)
+
+}
+    // loop {
+    //     let line = readline()?;
+    //     let line = line.trim();
+    //     if line.is_empty() {
+    //         continue;
+    //     }
+    //     match respond(line) {
+    //         Ok(resp) => {
+    //             match resp {
+    //                 Response::Quit => {
+    //                     break;
+    //                 }
+    //                 Response::MetaCommand => {}
+    //                 Response::Stmt => {
+    //                     let lexer = Lexer::new(line);
+    //                     let parser = ScriptParser::new();
+    //                     let ast = parser.parse(lexer).unwrap();
+    //
+    //                     for i in &ast {
+    //                         let mut pp = PrettyPrinter::new();
+    //                         pp.visit_stmt(i);
+    //                         // println!("{}", 1);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //         Err(err) => {
+    //             write!(std::io::stdout(), "{err}").map_err(|e| e.to_string())?;
+    //             std::io::stdout().flush().map_err(|e| e.to_string())?;
+    //         }
+    //     }
+    //     // let v: Vec<&str> = line.split(' ').collect();
+    //     // if line.eq("exit") {
+    //     //     break;
+    //     // }
+    //
+    //     // let lexer = Lexer::new(line);
+    //     // let parser = ScriptParser::new();
+    //     // let ast = parser.parse(lexer).unwrap();
+    //     //
+    //     // for i in &ast {
+    //     //     let mut pp = PrettyPrinter::new();
+    //     //     pp.visit_stmt(i);
+    //     //     // println!("{}", 1);
+    //     // }
+    // }
+    //
+    // Ok(())
+
+fn respond(line: &str) -> Result<Response, String> {
+    let args = shlex::split(line).ok_or("error: Invalid quoting")?;
+    let cli_res = Cli::try_parse_from(args).map_err(|e| e.to_string());
+    match cli_res {
+        Ok(cli) => {
+            match cli.command {
+                Commands::Ping => {
+                    write!(std::io::stdout(), "Pong").map_err(|e| e.to_string())?;
+                    std::io::stdout().flush().map_err(|e| e.to_string())?;
+                }
+                Commands::Exit => {
+                    write!(std::io::stdout(), "Exiting ...").map_err(|e| e.to_string())?;
+                    std::io::stdout().flush().map_err(|e| e.to_string())?;
+                    return Ok(Response::Quit);
+                }
+            }
+        },
+        // No metacommand
+        Err(_) => {
+            return Ok(Response::Stmt);
+        }
+    }
+    Ok(Response::MetaCommand)
 }
 
-#[derive(Debug)]
-enum CommandResponse {
-    Exit,
-    Pong,
-    Stmt(String)
+pub enum Response {
+    Quit,
+    MetaCommand,
+    Stmt(line)
 }
 
 #[derive(Parser, Debug)]
@@ -40,7 +121,7 @@ enum Commands {
 }
 
 pub fn readline() -> Result<String, String> {
-    write!(std::io::stdout(), "> ").map_err(|e| e.to_string())?;
+    write!(std::io::stdout(), "MartelDB > ").map_err(|e| e.to_string())?;
     std::io::stdout().flush().map_err(|e| e.to_string())?;
     let mut buffer = String::new();
     std::io::stdin()
